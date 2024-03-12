31ecf6d331a45b53a3a0ad79c132e914f9e3d7ce
Himani <him.papnai@gmail.com>
2024-03-12 19:24:20 +0530

    @himanipapani testing#

--- force-app/main/default/lwc/testcom/testcom.js
+++ force-app/main/default/lwc/testcom/testcom.js
@@ -1,3 +1,8 @@
 import { LightningElement } from 'lwc';
 
-export default class Testcom extends LightningElement {}
+export default class Testcom extends LightningElement {
+
+     handleSave(){
+        
+     }
+}
